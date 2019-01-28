5ea29559e38c2fa7a76798657f9b9fb8c7883894
dax <43710813+DavidsRepos@users.noreply.github.com>
2019-01-27 19:15:57 -0700

    rendered map

--- assets/javascript/google.js
+++ assets/javascript/google.js
@@ -1,33 +1,43 @@
-if (city === "" || city != null) {
-    $("#mapid").hide();
-    $("#Band-Name").focus();
-
-} 
-    else {
 
 
-  //adding the map function through Leaflet Issues#13
-  function addMap() {
-    $("#mapid").empty();
-    var mymap = L.map('mapid').setView([venueLat, venueLong], 13);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiYWFybTQ3MDIiLCJhIjoiY2pyM3RiNmw5MGU1bDN5bXk5MXE1ZGs2bSJ9.QbhjZk1rjfQb2fo7bvI-8A'
-        }).addTo(mymap);         
 
-    //getting the marker in map using the venue Name
-    var marker = L.marker([venueLat, venueLong]).addTo(mymap);
-    marker.bindPopup("<b>Venue</b><br>" + venueName).openPopup();
+// if (city === "") {
+//     $("#mapid").hide();
+//     $("#Band-Name").focus();
 
-    };
+// } 
+// else {
+   
 
-    $("#mapid").append(addMap);
+// $(".imageclick").on("click", function(){
 
-    // this always brings the cursor back to artist text field after submit
-    $("#Band-Name").focus();
+    // console.log(this);
+    // var latitude = $(this);
+    // console.log(latitude);
 
-} // end of if statement for city search
+    // function initMap() {
+    //     // The location of Uluru
+    //     var uluru = {lat: -25.344, lng: 131.036};
+    //     console.log(uluru);
+    //     // The map, centered at Uluru
+    //     var map = new google.maps.Map(
+    //         document.getElementById('mapid'), {zoom: 4, center: uluru});
+    //     // The marker, positioned at Uluru
+    //     var marker = new google.maps.Marker({position: uluru, map: map});
+    //   }
 
-$("#mapid").show();
+// });
+   
+    
+    // var googleAPI = "AIzaSyC9zfEWNWOOe7okPSVnnbBbkd4o2yVGhho";
+
+    // var googletURL = src="https://maps.googleapis.com/maps/api/js?key=" + googleAPI + "&callback=initMap";
+    
+//     $("#mapid").append(initMap);
+
+// //     // this always brings the cursor back to artist text field after submit
+// //     $("#Band-Name").focus();
+
+// // // end of if statement for city search
+
+// $("#mapid").show();
