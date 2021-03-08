ed4f201de994cd69459c8e9876c978e9df686b26
David S <43710813+DavidsRepos@users.noreply.github.com>
2021-03-08 16:33:30 -0700

    Update app.js
    modified to cycle out rotation of images

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    var count = 0, bg;
-    var $body = $("body");
-    var imageArray = [ 
-        assets/images/c0.jpg,
-        assets/images/c1.jpg
-    ];
+//     var count = 0, bg;
+//     var $body = $("body");
+//     var imageArray = [ 
+//         /images/c0.jpg,
+//         /images/c1.jpg
+//     ];
 
-    setInterval(function () {
-        if (count == 20)
-            count = 0;
-        bg = "url('" + imageArray[count] + "')";
-        count++;
-        $body.css({
-            "background-image": bg,
-            "background-size": "cover",
-            "transition": "1s",
-            "delay": "1s" 
-        });
+//     setInterval(function () {
+//         if (count == 20)
+//             count = 0;
+//         bg = "url('" + imageArray[count] + "')";
+//         count++;
+//         $body.css({
+//             "background-image": bg,
+//             "background-size": "cover",
+//             "transition": "1s",
+//             "delay": "1s" 
+//         });
         
-    }, 6000);
+//     }, 6000);
 
-}); //END document.ready
+// }); //END document.ready
