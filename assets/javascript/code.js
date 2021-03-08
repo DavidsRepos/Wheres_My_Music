e73bae101f59e80724ba53b8c4c76949ed38bd27
David S <43710813+DavidsRepos@users.noreply.github.com>
2021-03-08 15:42:20 -0700

    Update code.js

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -97,7 +97,7 @@ $.ajax({
         //Loop through for 10 event results print to screen
         for (i = 0; i < 10; i++) {
             
-        var eventName = response._embedded.events[i].name;
+        // var eventName = response._embedded.events[i].name;
         var eventURL = response._embedded.events[i].url;
         var localeventDate = response._embedded.events[i].dates.start.localDate;
         var localeventDateUnformatted = response._embedded.events[i].dates.start.localDate;
