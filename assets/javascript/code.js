42d17ae4f5db6425d43bae30142b8c65322089c4
David S <43710813+DavidsRepos@users.noreply.github.com>
2021-03-08 15:52:18 -0700

    Update code.js

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -94,14 +94,14 @@ $.ajax({
     //Process API Get response
     }).then(function(response){
         console.log("response:" + response);
-        //Loop through for 10 event results print to screen
+        // Loop through for 10 event results print to screen
         for (i = 0; i < 10; i++) {
             if (response._embedded.events[i].name === undefined) {
                alert("No Events Found for" + bandName)
             }
         var eventName = response._embedded.events[i].name;
         var eventURL = response._embedded.events[i].url;
-        var localeventDate = response._embedded.events[i].dates.start.localDate;
+        // var localeventDate = response._embedded.events[i].dates.start.localDate;
         var localeventDateUnformatted = response._embedded.events[i].dates.start.localDate;
         var localeventDate = moment(localeventDateUnformatted).format("MMMM Do YYYY")
         var localeventTime = response._embedded.events[i].dates.start.localTime;
